655c8fb01068f89ca9e609a64e8c64c8d84eb3b9
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-03-31 13:09:50 -0400

    Remove App.css and all references

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import Header from "./components/Header/Header";
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header />
       </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./Header.css";
 
 function Header(props) {
+  const { currentScore, topScore, guessResult } = props;
   return (
     <header className="header">
       <h1 id="brand">kitty klicker</h1>
       <div id="scoreboard">
         <h4>
-          Current Score: {props.currentScore ? props.currentScore : 0} | Top
-          Score: {props.topScore ? props.topScore : 0}
+          Current Score: {currentScore ? currentScore : 0} | Top
+          Score: {topScore ? topScore : 0}
         </h4>
-        <h4>{props.guess ? props.guess : "Click an image to begin!"}</h4>
+        <h5>{guessResult ? guessResult : "Click an image to begin!"}</h5>
       </div>
     </header>
   );
