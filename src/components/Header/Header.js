e08d5bc4ffcaac61b6982e92dd8eacab32507ef0
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-01 14:48:20 -0400

    Extract Ticker component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,22 @@
 import React from "react";
 import "./Header.css";
+import Ticker from "../Ticker/Ticker";
 
 function Header(props) {
-  const { currentScore, topScore, correct, wonGame } = props;
-  let ticker;
-
-  switch (correct) {
-    case true:
-      ticker = <h5>Nice! You haven't guessed that kitty yet.</h5>;
-      break;
-    case false:
-      ticker = <h5>Sorry! You already guessed that kitty. Try again!</h5>;
-      break;
-    default:
-      ticker = <h5>Click any image to get started!</h5>;
-      break;
-  }
+  const { currentScore, topScore, wonGame, correct } = props;
 
   return (
     <header className="header">
       <h1 id="brand">kitty klicker</h1>
       <div id="scoreboard">
         <h4>
-          Current Score: {currentScore ? currentScore : 0} | Top Score: {topScore ? topScore : 0}
+          Current Score: {currentScore ? currentScore : 0} | Top Score:{" "}
+          {topScore ? topScore : 0}
         </h4>
-        {ticker}
-        {wonGame ? <h5>Purrrrfect! You win! Click any image to play again.</h5> : null}
+        <Ticker correct={correct} />
+        {wonGame ? (
+          <h5>Purrrrfect! You win! Click any image to play again.</h5>
+        ) : null}
       </div>
     </header>
   );
