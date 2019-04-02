41390894c4426974aaa4a9ceee0f15702a6fd1cf
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-04-02 19:24:31 -0400

    Updates

---
+++ precache-manifest.58df9e087643533828fc8361a8662d81.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "453442fb9895152c7199",
+    "url": "/clickygame/static/css/main.3cd75816.chunk.css"
+  },
+  {
+    "revision": "453442fb9895152c7199",
+    "url": "/clickygame/static/js/main.6b76f37d.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/clickygame/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "9514a8a399b49f021dac2bae347fa961",
+    "url": "/clickygame/static/media/kitten1.9514a8a3.jpg"
+  },
+  {
+    "revision": "b8bf39b182a351e87366c28e4ff2916a",
+    "url": "/clickygame/static/media/kitten3.b8bf39b1.jpg"
+  },
+  {
+    "revision": "f3a3980ee4d4ea20ebec2f5250771480",
+    "url": "/clickygame/static/media/kitten5.f3a3980e.jpg"
+  },
+  {
+    "revision": "569cc2090e544eaae8acdc6b8f370763",
+    "url": "/clickygame/static/media/kitten6.569cc209.jpg"
+  },
+  {
+    "revision": "791df6787e06c04a2f7cea16b20e644e",
+    "url": "/clickygame/static/media/kitten9.791df678.jpg"
+  },
+  {
+    "revision": "8ee30425025d55af46fa50e510b71fcc",
+    "url": "/clickygame/static/media/kitten11.8ee30425.jpg"
+  },
+  {
+    "revision": "e7f52c02c4ddcbdd188835c799810ef0",
+    "url": "/clickygame/static/media/kitten12.e7f52c02.jpg"
+  },
+  {
+    "revision": "45593bf6bcec622c3f9df684458719bb",
+    "url": "/clickygame/index.html"
+  }
+];
